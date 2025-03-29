2937867c3983627d8eb33b2ae290a2168cf66231
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-03-29 23:11:41 +0000

    added typing animation in card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,16 @@
 import { motion, AnimatePresence } from "framer-motion";
+import { Repeat } from "lucide-react";
 import { useState, useEffect } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 export const Card = () => {
   const messages = ["Eid Mubarak", "عيد مبارك"];
   const [index, setIndex] = useState(0);
+  const [timer, setTimer] = useState(false);
+
+  setTimeout(() => {
+    setTimer(true);
+  }, 3000);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -15,7 +22,7 @@ export const Card = () => {
   return (
     <motion.div
       className="flex h-[200px] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-xl border border-slate-700 bg-slate-900 px-4 text-center shadow-xl sm:w-[300px] md:w-[500px]"
-      initial={{ y: 200, opacity: 0}}
+      initial={{ y: 200, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ duration: 1.3, ease: "easeOut" }}
     >
@@ -26,14 +33,33 @@ export const Card = () => {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: .5 }}
+          transition={{ duration: 0.5 }}
         >
           {messages[index]}
         </motion.p>
       </AnimatePresence>
-      <label className="mt-2 cursor-pointer text-xl text-white">
-        May Allah Bless You And Your Family
-      </label>
+      {timer && (
+        <TypeAnimation
+          sequence={[
+            "May Allah Bless You And Your Family",
+            1500,
+            "May Allah Accept Your Good Deeds",
+            1500,
+            "May Allah Forgive Your Sins",
+            1500,
+            "May Allah Increase Your Iman",
+            1500,
+            "May Allah Shower His Mercy Upon You",
+            1500,
+            "May Allah Save Us From The Hell Fire",
+            1500,
+          ]}
+          speed={20}
+          repeat={Infinity}
+          deletionSpeed={20}
+          className="mt-2 cursor-pointer text-xl text-white"
+        ></TypeAnimation>
+      )}
     </motion.div>
   );
 };
